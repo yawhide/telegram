5cde2a7ff1fa7f83d7c2f1bdbc1a6414004870c8
Shayan Masood <zmasood.shayan@gmail.com>
2016-06-14 00:07:10 -0400

    Server add interim - still slightly broken

--- orient.py
+++
@@ -1,7 +0,0 @@
-import pyorient
-
-client = pyorient.OrientDB("localhost", 2480)
-session_id = client.connect( "root", "telegram123")
-
-client.db_open( "telegram", "root", "telegram123" )
-
--- server.py
+++ server.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, request
+from pymongo import MongoClient, GEO2D
+
 app = Flask(__name__)
+master_db = 'test_telgram'
+
+class Telegram (object):
+  def __init__ (self, tid, uid, msg, img, lat, lng):
+    self.tid = tid
+    self.uid = uid
+    self.msg = msg
+    self.img = img
+    self.loc = {"lon": lng, "lat": lat} 
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/drop', methods=['POST'])
+def drop_telegram():
+  tid = request.args.get('tid')
+  uid = request.args.get('uid')
+  msg = request.args.get('msg')
+  img = request.args.get('img')
+  lat = request.args.get('lat')
+  lng = request.args.get ('lng')
+
+  telegram = Telegram (tid, uid, msg, img, lat, lng)
+  client = MongoClient('localhost', 27017)
+  db = client[master_db]
+
+  db.save(telegram.__dict__)
+  
+  return 'inserted telegram' 
+
 if __name__ == "__main__":
-    app.run()
     app.run(host='0.0.0.0', port=5000)
