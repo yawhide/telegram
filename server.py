d6056b0d9fe35f4047c50d6c622f2981130406b8
Stefan Wojcik <ert.mcscrad@gmail.com>
2016-06-26 15:02:17 -0400

    dont upload an empty string!

--- server.py
+++ server.py
@@ -106,14 +106,15 @@ def drop_telegram():
   img = request.form.get('img')
   lat = request.form.get('lat')
   lng = request.form.get ('lng')
+  imgUrl = ''
+  if (img) {
+    s3key = str(uuid4())
+    k = Key(bucket)
+    k.key = s3key
+    k.set_contents_from_string(img)
 
-  s3key = str(uuid4())
-
-  k = Key(bucket)
-  k.key = s3key
-  k.set_contents_from_string(img)
-
-  imgUrl = 'https://s3-us-west-2.amazonaws.com/telegramimages/' + s3key
+    imgUrl = 'https://s3-us-west-2.amazonaws.com/telegramimages/' + s3key
+  }
 
   telegram = Telegram (uid, msg, imgUrl, lat, lng)
   result = db.telegrams.insert_one(telegram.__dict__)
